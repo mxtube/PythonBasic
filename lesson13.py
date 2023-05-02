2d8968aadcedacb37e75e480f4a9c79458c06638
Kirill Kuznetsov <kafomin@yandex.ru>
2023-05-02 18:10:12 +0300

    lesson 13

---
+++ lesson13.py
@@ -0,0 +1,40 @@
+"""
+Lesson 13 - Cycle For and While
+02.05.2023 @ Kirill Kuznetsov
+"""
+
+# Example 1 - Endless cycle
+while True:
+    print('Hello Python!')
+    break
+
+i = 1
+while i <= 10:
+    print(i, end=' ')
+    # i = i + 1
+    i += 1
+
+# Example 2 - For cycle
+s = 'Hello Python'
+for l in s:
+    if l == ' ':
+        continue;
+    print(f'"{l}"', end=' ')
+
+# Example 3 - Operator break
+for i in 'Hello Python':
+    if i == ' ':
+        break
+    print(i, end=' ')
+
+# Example 4 - Operator else
+for i in 'Hello Python':
+
+    print(i, end=' ')
+else:
+    print('\nNo spaces')
+
+# Homework
+# Create a list by year from 1900 to 2023
+for year in range(1900, 2023):
+    print(year)
